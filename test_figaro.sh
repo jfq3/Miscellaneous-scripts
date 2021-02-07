139c5b7f22f17d47462589f211246c215695a360
jfq3 <jfq3@users.noreply.github.com>
2021-02-07 09:22:41 -0500

    Add files via upload
    Use this script to test your installation of FIGARO after following the instructions at https://john-quensen.com/tutorials/figaro/.

---
+++ test_figaro.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Test FIGARO installation
+# Activate the FIGARO environment
+source ~/miniconda3/etc/profile.d/conda.sh # If necessary for your conda installation.
+conda activate figaro
+
+cd 
+mkdir test_figaro
+cd test_figaro
+
+# Download example files from the QIIME2 tutorial pages
+wget "https://data.qiime2.org/2020.2/tutorials/atacama-soils/1p/forward.fastq.gz"
+wget "https://data.qiime2.org/2020.2/tutorials/atacama-soils/1p/reverse.fastq.gz"
+ 
+ # Decompress
+ gzip -d *.fastq.gz
+ 
+ # Rename the files in Zymo format
+ mv forward.fastq sam1_16s_R1.fastq
+ mv reverse.fastq sam1_16s_R2.fastq
+ 
+ # Run FIGARO
+ # cd to installation folder
+ cd ~/figaro
+ python figaro.py -i ~/test_figaro/ -o ~/test_figaro/ -f 10 -r 10 -a 253 -F zymo
+ 
+ conda deactivate
